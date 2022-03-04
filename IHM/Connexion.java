cc7b6be825eddd4c2c8464701a6089fef6b413a7
MaximeHocquet <79260077+MaximeHocquet@users.noreply.github.com>
2022-03-04 19:38:00 +0100

    Add files via upload

--- IHM/Connexion.java
+++ IHM/Connexion.java
@@ -4,19 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.RenderingHints;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,11 +23,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-
-import org.primefaces.component.button.Button;
+import javax.swing.WindowConstants;
 
 import data.Utilisateur;
-import net.sf.cglib.asm.Label;
 import requete.GestionUtilisateur;
 
 
@@ -38,61 +35,64 @@ public class Connexion extends JFrame
 {
 	private Utilisateur utilisateur;
 	private static JPanel global;
-	private JPanel section, connexion;
+	private JPanel icon ,connexion;
 	private JPanel log1, log2, controls;
+	private JLabel iconSportsL;
 	
 	private JButton connect, create;
-	
+
 	private JTextField login;
 	private JPasswordField password;
 	
+	private Color background = new Color(63, 149, 245);
 	private int width = 500;
-	private int height = 500;
-	
-	
+	private int height = 400;
+
+
 	/**
 	 * Constructeur de la classe Fenetre.
-	 * 
+	 *
 	 * Ce constructeur gère la disposition des éléments de l'accueil
 	 */
 	public Connexion(){
-		this.setTitle("Test Sport2");
-		this.setSize(width, height);
+		this.setTitle("Sport2");
+		this.setPreferredSize(new Dimension(width, height));
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		this.setLayout(new FlowLayout());
-		
+
 		//-------------- CONTENT PANE ------------------//
 		global = new JPanel();
 	    global.setPreferredSize(new Dimension(width, height));
-		global.setBackground(Color.CYAN);
-		global.setLayout(new BorderLayout());
-		this.setContentPane(global);
+		global.setBackground(background);
+		global.setLayout(new FlowLayout());
 		
-		
-		//-------------- CONNECT PAGE ------------------//		
+
+		//-------------- CONNECT PAGE ------------------//
 		displayElements();
+		
+		this.setContentPane(global);
 		
 		this.setVisible(true);
 		pack();
 	}
-	
+
 
 	private void displayElements()
 	{
-		JPanel back = new JPanel();
+		/*JPanel back = new JPanel();
 		back.setSize(width, height);
 		back.setBackground(Color.WHITE);
-		back.setLayout(null);
-		global.add(back);		
+		back.setLayout(new FlowLayout());
+		global.add(back);*/
 		
-		section = new JPanel();
+		/*section = new JPanel();
 		section.setBounds(100, 50, width-200, height-200);
 		section.setBackground(Color.WHITE);
 		section.setLayout(new GridLayout(3, 1));
 		back.add(section);
-		
+
 	/*	header = new JPanel();
 		header.setBackground(Color.WHITE);
 		header.setLayout(new BorderLayout());
@@ -104,79 +104,107 @@ public class Connexion extends JFrame
 		
 		connexion = new JPanel();
 		connexion.setBackground(Color.WHITE);
-		connexion.setLayout(new GridLayout(3, 1));		
+		connexion.setPreferredSize(new Dimension(300,75));
+		connexion.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+		connexion.setLayout(new GridLayout(2, 1));
 
 		log1 = new JPanel();
 		log1.setBackground(Color.WHITE);
-		log1.add(new JLabel("login : "));
+		log1.setLayout(new BorderLayout());
+		log1.add(new JLabel("Login : "),BorderLayout.WEST);
 		login = new JTextField(17);
-		log1.add(login);
-		
+		log1.add(login,BorderLayout.EAST);
+
 		log2 = new JPanel();
 		log2.setBackground(Color.WHITE);
-		log2.add(new JLabel("Mot de passe : "));
+		log2.setLayout(new BorderLayout());
+		log2.add(new JLabel("Password : "),BorderLayout.WEST);
 		password = new JPasswordField(17);
 		password.setBackground(Color.WHITE);
 		//password.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 		//password.setFont(new Font("Verdana", Font.PLAIN, 16));
 		//password.setForeground(Color.WHITE);
 		//password.setCaretColor(Color.WHITE);
-		log2.add(password);
-		
-		controls = new JPanel();
-		controls.setBackground(Color.WHITE);
-		controls.setLayout(new FlowLayout());
-		
-		connect = new JButton("Se connecter");
-		create = new JButton("Créer un compte");		
-		controls.add(connect);
-		controls.add(create);
+		log2.add(password,BorderLayout.EAST);
 		
 		connexion.add(log1);
 		connexion.add(log2);
-		connexion.add(controls);
-		section.add(connexion);
+
+		controls = new JPanel();
+		controls.setBackground(background);
+		controls.setPreferredSize(new Dimension(400,80));
+		controls.setLayout(new FlowLayout(10, 62, 20));
+
+		connect = new JButton("Se connecter");
+		connect.setBackground(Color.WHITE);
+		connect.setBorder(BorderFactory.createLineBorder(Color.WHITE, 10));
+		
+		create = new JButton("Créer un compte");
+		create.setBackground(Color.WHITE);
+		create.setBorder(BorderFactory.createLineBorder(Color.WHITE, 10));
+		
+		controls.add(connect);
+		controls.add(create);
+
+	
+		//connexion.add(controls);
+		
+		BufferedImage myPicture;
+		try {
+			URL url = getClass().getResource("../images/sports.jpg");
+			File file = new File(url.getPath());
+			myPicture = ImageIO.read(file);
+			iconSportsL = new JLabel(new ImageIcon(myPicture));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		icon = new JPanel();
+		icon.setBackground(background);
+		icon.setLayout(new FlowLayout());
+		icon.add(iconSportsL);
+
+		global.add(icon);
+		global.add(connexion);
+		global.add(controls);
 		
 		connect.addActionListener(new ButtonListener());
 		create.addActionListener(new ButtonListener());
 	}
-	
+
 	/**
 	 * Cette méthode permet de réinitialiser l'accueil
 	 */
-	
-	
-	
-	
 
-	
-	
+
 	/**
 	 * Ecouteur associé aux boutons "cycle", "proteine" et "heritage" du panneau de commandes.
-	 * 
+	 *
 	 * Chacun des boutons déclenche l'affichage du panel d'accès aux activités
 	 */
 	private class ButtonListener implements ActionListener
 	{
+		@Override
 		@SuppressWarnings("deprecation")
-		public void actionPerformed(ActionEvent e) 
+		public void actionPerformed(ActionEvent e)
 		{
 			int id_utilisateur;
 			if(e.getSource() == connect)
 			{
 				GestionUtilisateur um = new GestionUtilisateur();
 				utilisateur = um.findUser(login.getText(), password.getText());
-				
+
 				id_utilisateur = um.findID(login.getText(), password.getText());
 				if(utilisateur == null)
 					JOptionPane.showMessageDialog(null, "Utilisateur inexistant");
 				else
 				{
 					//global.removeAll();
-					
+
 					new Menu(id_utilisateur);
 				}
-				
+
 			}
 			else if(e.getSource() == create)
 			{
@@ -186,38 +214,38 @@ public class Connexion extends JFrame
 				pageInscription();
 			}
 
-			//global.revalidate();	
+			//global.revalidate();
 		}
 	}
-	
+
 	public void pageInscription(){
-		
-		
+
+
 		new Inscription();
 		this.setVisible(false);
-		
+
 	}
-	
+
 	/*public void pageMenu()
 	{
 		//global.add(new Connexion(), BorderLayout.CENTER);
 		//global.repaint();
-		
+
 		new Menu(id_utilisateur);
 		this.setVisible(false);
-		
 
-		
+
+
 	}*/
-	
-		
-	
-	
+
+
+
+
 	public static void main (String [] args) {
 		new Connexion();
 	}
-		
-	
-	
+
+
+
 
 }
--- IHM/Inscription.java
+++ IHM/Inscription.java
@@ -1,136 +1,153 @@
 package IHM;
 
-import javax.swing.JTextField;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.jfree.ui.RefineryUtilities;
-
-import bd_init.DBConnection;
-import data.Utilisateur;
-import graph.GraphGolfPoint;
-import requete.InsertGolf;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.AbstractButton;
-import javax.swing.ButtonGroup;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import java.awt.GridLayout;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
+import bd_init.DBConnection;
+import data.Utilisateur;
 
 public class Inscription extends JFrame {
   private JPanel container = new JPanel();
-  private JTextField nom = new JTextField();
-  private JLabel labelNom = new JLabel("Nom : ");
-  private JTextField prenom = new JTextField();
-  private JLabel labelPrenom = new JLabel("Prenom : ");
-  private JTextField naissance = new JTextField();
-  private JLabel labelNaissance = new JLabel("date de naissance : ");
-  private JTextField login = new JTextField();
-  private JLabel labelLogin = new JLabel("login : ");
-  private JPasswordField password = new JPasswordField();
-
-  private JLabel labelPassword = new JLabel("password : ");
-  private  GridLayout  grid = new GridLayout(6, 0);
+  private JPanel inscription = new JPanel();
   
+  private JPanel nomP = new JPanel();
+  private JTextField nom = new JTextField(17);
+  private JLabel labelNom = new JLabel("Nom : ");
+  
+  private JPanel prenomP = new JPanel();
+  private JTextField prenom = new JTextField(17);
+  private JLabel labelPrenom = new JLabel("Prenom : ");
+  
+  private JPanel naissanceP = new JPanel();
+  private JTextField naissance = new JTextField(17);
+  private JLabel labelNaissance = new JLabel("Date de naissance : ");
+  
+  private JPanel loginP = new JPanel();
+  private JTextField login = new JTextField(17);
+  private JLabel labelLogin = new JLabel("Login : ");
+  
+  private JPasswordField password = new JPasswordField(17);
+  private JPanel passwordP = new JPanel();
+  private JLabel labelPassword = new JLabel("Password : ");
+  
+  private JPanel buttonsP = new JPanel();
   private JButton btnSubmit,retour;
 
-
+  private Color background = new Color(63, 149, 245);
+  private int width = 500;
+  private int height = 400;
 
 
   public Inscription(){
 	  
-	    this.setTitle("Animation");
-	    this.setSize(300, 300);
-	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setLocationRelativeTo(null);
-	    container.setBackground(Color.white);
-	    container.setLayout(grid);
+    this.setTitle("Sports2");
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	this.setSize(new Dimension(width, height));
+    this.setLocationRelativeTo(null);
+    this.setResizable(false);
+    this.setLayout(new FlowLayout());
     
-    container.setBackground(Color.white);
-    
-   
     Font police = new Font("Arial", Font.BOLD, 14);
     
+    container.setPreferredSize(new Dimension(width, height));
+    container.setBackground(background);
+   // container.setLayout(new GridLayout(6,1));
+    container.setLayout(new FlowLayout(10, 50, 20));
+    
+    inscription.setLayout(new GridLayout(5,1));
+    inscription.setPreferredSize(new Dimension(380, 180));
+    
     nom.setFont(police);
-    nom.setPreferredSize(new Dimension(150, 30));
-    
     prenom.setFont(police);
-    prenom.setPreferredSize(new Dimension(150, 30));
-    
     naissance.setFont(police);
-    naissance.setPreferredSize(new Dimension(150, 30));
-    
     login.setFont(police);
-    login.setPreferredSize(new Dimension(150, 30));
-    
     password.setFont(police);
-    password.setPreferredSize(new Dimension(150, 30));
+
+    nomP.setLayout(new BorderLayout());
+    nomP.add(labelNom,BorderLayout.WEST);
+    nomP.add(nom,BorderLayout.EAST);
     
+    prenomP.setLayout(new BorderLayout());
+    prenomP.add(labelPrenom,BorderLayout.WEST);
+    prenomP.add(prenom,BorderLayout.EAST);
     
-  
-    container.add(labelNom);
-    container.add(nom);
-    
-    container.add(labelPrenom);
-    container.add(prenom);
-    
-    container.add(labelNaissance);
-    container.add(naissance);
-    
-    container.add(labelLogin);
-    container.add(login);
-    
-    container.add(labelPassword);
-    container.add(password);
-    
+    naissanceP.setLayout(new BorderLayout());
+    naissanceP.add(labelNaissance,BorderLayout.WEST);
+    naissanceP.add(naissance,BorderLayout.EAST);
+
+    loginP.setLayout(new BorderLayout());
+    loginP.add(labelLogin,BorderLayout.WEST);
+    loginP.add(login,BorderLayout.EAST);
+
+    passwordP.setLayout(new BorderLayout());
+    passwordP.add(labelPassword,BorderLayout.WEST);
+    passwordP.add(password,BorderLayout.EAST);
+
+    buttonsP.setBackground(background);
+    buttonsP.setPreferredSize(new Dimension(400,80));
+    buttonsP.setLayout(new FlowLayout(10, 90, 30));
     
     btnSubmit = new JButton("Submit");
-	btnSubmit.setBounds(50, 10, 124, 21);
-	container.add(btnSubmit);
-	
+	//btnSubmit.setBounds(50, 10, 124, 21);
+	btnSubmit.setBackground(Color.WHITE);
+	btnSubmit.setBorder(BorderFactory.createLineBorder(Color.WHITE, 10));
+	buttonsP.add(btnSubmit);
+
 	retour = new JButton("Cancel");
-	retour.setBounds(184, 10, 85, 21);
-	container.add(retour);
-	
+	//retour.setBounds(184, 10, 85, 21);
+	retour.setBackground(Color.WHITE);
+	retour.setBorder(BorderFactory.createLineBorder(Color.WHITE, 10));
+	buttonsP.add(retour);
+
 	btnSubmit.addActionListener(new AjoutUtilisateur());
 	retour.addActionListener(new AjoutUtilisateur());
-
+	
+	inscription.add(nomP);
+	inscription.add(prenomP);
+	inscription.add(naissanceP);
+	inscription.add(loginP);
+	inscription.add(passwordP);
+	
+	container.add(inscription);
+	container.add(buttonsP);
+	
     this.setContentPane(container);
-    this.setVisible(true);            
+    this.setVisible(true);
   }
-  
+
   public class AjoutUtilisateur implements ActionListener {
-		
+
+		@Override
 		public void actionPerformed(ActionEvent e1) {
 			String naissanceT=naissance.getText();
 			String nomT=nom.getText();
 			String prenomT=prenom.getText();
 			String loginT=login.getText();
 			String passwordT=password.getText();
-		
-			
-			
-			
-			
+
+
+
+
+
 				if(e1.getSource()== btnSubmit) {
 					if (naissance.getText().isEmpty() && nom.getText().isEmpty() && prenom.getText().isEmpty() && login.getText().isEmpty() && password.getText().isEmpty() ) {
 						JOptionPane.showMessageDialog(null, "Veuillez saisir tous les champs");
@@ -141,31 +158,31 @@ public class Inscription extends JFrame {
 						Transaction persistTransaction1 = session.beginTransaction();
 						Utilisateur u1 = new Utilisateur(nomT,prenomT,naissanceT,loginT,passwordT);
 						session.save(u1);
-					
+
 					}
 				}
-			
+
 				if(e1.getSource() == retour) {
-					retour();					
+					retour();
 				}
-				
+
 			}
 		}
-				
-	
-	
+
+
+
 	public void retour()
 	{
 		//global.add(new Connexion(), BorderLayout.CENTER);
 		//global.repaint();
-		
+
 		new Connexion();
 		this.setVisible(false);
-		
 
-		
+
+
 	}
-	
-		
-		
+
+
+
 }
