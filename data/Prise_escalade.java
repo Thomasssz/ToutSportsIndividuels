5745014317693d97c36172324ba19f00855f6d2c
MaximeHocquet <79260077+MaximeHocquet@users.noreply.github.com>
2022-03-25 22:25:03 +0100

    Add files via upload

--- data/Prise_escalade.java
+++ data/Prise_escalade.java
@@ -9,17 +9,48 @@ import javax.persistence.Id;
 public class Prise_escalade {
 	@Id
 	@GeneratedValue
-	private int id_kilometre;
+	private int id_prise;
 	
-	private float kilometre;
-	private int jour;
-	
+	private int block;
+	private int prisesMin;
+	private int prises;
 	
 	public Prise_escalade() {
 	}
+	public Prise_escalade(int block, int prisesMin, int prises) {
+		this.block = block;
+		this.prisesMin = prisesMin;
+		this.prises = prises;
+	}
+	public int getId_prise() {
+		return id_prise;
+	}
+	public void setId_prise(int id_prise) {
+		this.id_prise = id_prise;
+	}
+	public int getPrisesMin() {
+		return prisesMin;
+	}
+	public void setPrisesMin(int prisesMin) {
+		this.prisesMin = prisesMin;
+	}
+	public int getPrises() {
+		return prises;
+	}
+	public void setPrises(int prises) {
+		this.prises = prises;
+	}
+	public int getBlock() {
+		return block;
+	}
+	public void setBlock(int block) {
+		this.block = block;
+	}
+	@Override
+	public String toString() {
+		return "Prise_escalade [id_prise=" + id_prise + ", block=" + block + ", prisesMin=" + prisesMin + ", prises=" + prises + "]";
+	}
 	
-	
-	
-	
-	//a definir
+
+
 }
